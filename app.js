dd9d8fd2bdd8c2db430acf10fc46a71a2f07ec7f
Erenen1 <erencelikk212@gmail.com>
2024-10-14 22:27:44 +0300

    if block fixed

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-if (process.env.NODE_ENV != "test") {
+
+if (process.env.NODE_ENV !== "test") {
   app.listen(PORT, () => {
     console.log(`Server ${PORT} üzerinden çalışıyor.`);
   });
