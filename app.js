4a74722660b6fa50fd16d07cbedd9e4c6c467e27
Erenen1 <erencelikk212@gmail.com>
2024-10-14 22:26:21 +0300

    production, development and test environments seperated

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config({
+  path: `.env.${process.env.NODE_ENV}`
+})
 
 app.get('/', (req, res) => {
   res.send('Merhaba Dünya!');
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV != "test") {
+  app.listen(PORT, () => {
+    console.log(`Server ${PORT} üzerinden çalışıyor.`);
+  });
+}
 
 module.exports = app;
--- test/app.test.js
+++ test/app.test.js
@@ -1,8 +1,22 @@
 const request = require('supertest');
 const app = require('../app');
 
+let server; // Sunucuyu burada tanımlıyoruz
+
+beforeAll(() => {
+  // Sunucuyu test öncesinde başlat
+  server = app.listen(4000, () => {
+    console.log('Test server 4000 portunda çalışıyor');
+  });
+});
+
+afterAll(() => {
+  // Test bittikten sonra sunucuyu kapat
+  server.close();
+});
+
 describe('GET /', () => {
-  it('should return Merhaba Dünya!', async () => {
+  it('"Merhaba Dünya!" dönmeli', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
     expect(res.text).toBe('Merhaba Dünya!');
